a0af5a996243442ee4900b8e9340f464538a9488
harsh <harsh164>
2025-04-15 16:42:13 +0530

    updated

--- script.js
+++ script.js
@@ -62,10 +62,22 @@ function scrollToTop() {
 
 
   
-  document.querySelectorAll(".service-box").forEach(box => {
-    box.addEventListener("click", function () {
-      if (window.innerWidth <= 768) {
-        this.classList.toggle("show-info");
-      }
-    });
-  });
+ 
+
+//   const cursor = document.querySelector(".cursor");
+// let mouseX = 0, mouseY = 0;
+// let cursorX = 0, cursorY = 0;
+
+// document.addEventListener("mousemove", (e) => {
+//   mouseX = e.clientX;
+//   mouseY = e.clientY;
+// });
+
+// function animate() {
+//   cursorX += (mouseX - cursorX) * 0.1;
+//   cursorY += (mouseY - cursorY) * 0.1;
+//   cursor.style.left = `${cursorX}px`;
+//   cursor.style.top = `${cursorY}px`;
+//   requestAnimationFrame(animate);
+// }
+// animate();
